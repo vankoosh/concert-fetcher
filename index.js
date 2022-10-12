d111d198ef25793f6c0641fab0a73d4418100344
vankoosh <vankoosh@gmail.com>
2022-10-12 21:20:17 +0000

    basic skeleton done

---
+++ index.js
@@ -0,0 +1,16 @@
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const text = document.querySelector(".text-field");
+const value = input.value;
+
+btn.addEventListener("click", (value) => {
+  if (value) {
+    const promise = new Promise(resolve, reject){
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+  }
+})
