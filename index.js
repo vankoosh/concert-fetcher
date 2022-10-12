85a23246970baeffe358fee80f66e7edfbf6a340
vankoosh <vankoosh@gmail.com>
2022-10-12 22:14:34 +0000

    fetch

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 const text = document.querySelector(".text-field");
 const value = input.value;
+const seatGeekID = "Mjk2Nzk4NjZ8MTY2NTYwNTAzNS44MTE0Mzkz";
 
-btn.addEventListener("click", (value) => {
-  if (value) {
-    const promise = new Promise(resolve, reject){
-      try {
-        
-      } catch (error) {
-        
-      }
-    }
+
+const getData = async () => {
+  const baseURL = "https://api.seatgeek.com/2/events?client_id=" + seatGeekID;
+try {
+  const response = await fetch(baseURL);
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
   }
-})
+} catch (error) {
+  console.log(error);
+}
+}
+
+btn.addEventListener("click", getData);
