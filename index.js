38758ffb4cd899c9fdac996d879e46522359cce1
vankoosh <vankoosh@gmail.com>
2022-10-14 15:44:12 +0000

    async not working

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 const text = document.querySelector(".text-field");
-const value = input.value;
+
 const endpoint = "https://api.spotify.com/v1/artists/";
-const id = "7xjpPNWngnYl57VxpnbakE";
+const artistID = "7xjpPNWngnYl57VxpnbakE";
 
-const getData = async (id) => {
-  const urlToFetch = `${endpoint}${id}`;
+const clientSecret = "f243d41ae73d46008dd9b66560b1b793";
+const clientID = "fb3eaa877de7469696f8a2e8eea738a7";
 
-  try {
-    const response = await fetch(urlToFetch);
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      console.log(jsonResponse);
-    }
-  } catch (error) {
-    console.log(error);
-  }
+const getToken = async () => {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientID + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await response.json();
+  return data.access_token;
+};
+
+const token = await getToken();
+
+const getData = async () => {
+  const result = await fetch(`${endpoint}${artistID}`, {
+    method: "GET",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  const data = await result.json();
+  console.log(data);
+
 };
 
 btn.addEventListener("click", getData);
