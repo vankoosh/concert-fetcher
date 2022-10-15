d1204cb3507a8e771117629787de4617d39ded62
vankoosh <vankoosh@gmail.com>
2022-10-15 16:18:53 +0000

    new API

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
+import axios from "axios";
+
 const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 const text = document.querySelector(".text-field");
 
+
 const options = {
   method: "GET",
-  url: "https://concerts-artists-events-tracker.p.rapidapi.com/artist",
-  params: { name: "depeche mode", page: "1" },
+  url: "https://porn-gifs.p.rapidapi.com/threesome",
   headers: {
     "X-RapidAPI-Key": "561470941fmsh0bedd09cf8b88d6p190531jsna84df3a8992b",
-    "X-RapidAPI-Host": "concerts-artists-events-tracker.p.rapidapi.com",
+    "X-RapidAPI-Host": "porn-gifs.p.rapidapi.com",
   },
 };
 
-const getData = () => {
-  axios(options)
-    .then((response)=> {
-      text.innerText = response.data;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  
-
-};
-
-const post = () => {
-  console.log("clicked")
-}
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 
 btn.addEventListener("click", getData);
