431a2485431999cac783371b6da3f9ed957e2905
vankoosh <vankoosh@gmail.com>
2022-10-15 16:12:35 +0000

    api limit of 10 reached

--- index.js
+++ index.js
@@ -2,37 +2,30 @@ const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 const text = document.querySelector(".text-field");
 
-const endpoint = "https://api.spotify.com/v1/artists/";
-const artistID = "7xjpPNWngnYl57VxpnbakE";
-
-const clientSecret = "f243d41ae73d46008dd9b66560b1b793";
-const clientID = "fb3eaa877de7469696f8a2e8eea738a7";
-
-const getToken = async () => {
-  const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "GET",
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded",
-      Authorization: "Basic " + btoa(clientID + ":" + clientSecret),
-    },
-    body: "grant_type=client_credentials",
-  });
-
-  const data = await response.json();
-  return data.access_token;
+const options = {
+  method: "GET",
+  url: "https://concerts-artists-events-tracker.p.rapidapi.com/artist",
+  params: { name: "depeche mode", page: "1" },
+  headers: {
+    "X-RapidAPI-Key": "561470941fmsh0bedd09cf8b88d6p190531jsna84df3a8992b",
+    "X-RapidAPI-Host": "concerts-artists-events-tracker.p.rapidapi.com",
+  },
 };
 
-const token = await getToken();
-
-const getData = async () => {
-  const result = await fetch(`${endpoint}${artistID}`, {
-    method: "GET",
-    headers: { Authorization: "Bearer " + token },
-  });
-
-  const data = await result.json();
-  console.log(data);
+const getData = () => {
+  axios(options)
+    .then((response)=> {
+      text.innerText = response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  
 
 };
+
+const post = () => {
+  console.log("clicked")
+}
 
 btn.addEventListener("click", getData);
