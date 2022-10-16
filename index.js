e2e941afc42dc1fcb9836b06092c4bf4830a322a
vankoosh <vankoosh@gmail.com>
2022-10-16 21:42:21 +0000

    fetch text t ospeech

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-import axios from "axios";
-
 const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 const text = document.querySelector(".text-field");
 
+const encodedParams = new URLSearchParams();
+encodedParams.append("src", "Hello, world!");
+encodedParams.append("hl", "en-us");
+encodedParams.append("r", "0");
+encodedParams.append("c", "mp3");
+encodedParams.append("f", "11khz_16bit_mono");
 
 const options = {
-  method: "GET",
-  url: "https://porn-gifs.p.rapidapi.com/threesome",
+  method: "POST",
   headers: {
+    "content-type": "application/x-www-form-urlencoded",
     "X-RapidAPI-Key": "561470941fmsh0bedd09cf8b88d6p190531jsna84df3a8992b",
-    "X-RapidAPI-Host": "porn-gifs.p.rapidapi.com",
+    "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com",
   },
+  body: encodedParams,
 };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
 
-btn.addEventListener("click", getData);
+
+btn.addEventListener("click", () => {
+    fetch(
+      "https://voicerss-text-to-speech.p.rapidapi.com/?key=7d510295baf84983bc8d1e26773f7b22",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  })
