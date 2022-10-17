7bac10d0783ed9d28d02c8826095c7855143b340
vankoosh <vankoosh@gmail.com>
2022-10-17 15:24:47 +0000

    still not working

--- index.js
+++ index.js
@@ -1,32 +1,116 @@
-const input = document.querySelector(".input");
-const btn = document.querySelector(".btn");
-const text = document.querySelector(".text-field");
 
-const encodedParams = new URLSearchParams();
-encodedParams.append("src", "Hello, world!");
-encodedParams.append("hl", "en-us");
-encodedParams.append("r", "0");
-encodedParams.append("c", "mp3");
-encodedParams.append("f", "11khz_16bit_mono");
+// const input = document.querySelector(".input");
+// const btn = document.querySelector(".btn");
+// const text = document.querySelector(".text-field");
 
-const options = {
-  method: "POST",
-  headers: {
-    "content-type": "application/x-www-form-urlencoded",
-    "X-RapidAPI-Key": "561470941fmsh0bedd09cf8b88d6p190531jsna84df3a8992b",
-    "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com",
-  },
-  body: encodedParams,
+// const encodedParams = new URLSearchParams();
+// encodedParams.append("src", "Hello, world!");
+// encodedParams.append("hl", "en-us");
+// encodedParams.append("r", "5");
+// encodedParams.append("c", "mp3");
+// encodedParams.append("f", "8khz_8bit_mono");
+
+// const options = {
+//   method: "POST",
+//   url: "https://voicerss-text-to-speech.p.rapidapi.com/",
+//   params: { key: "7d510295baf84983bc8d1e26773f7b22" },
+//   headers: {
+//     "content-type": "application/x-www-form-urlencoded",
+//     "X-RapidAPI-Key": "561470941fmsh0bedd09cf8b88d6p190531jsna84df3a8992b",
+//     "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com",
+//   },
+//   data: encodedParams,
+// };
+
+// btn.addEventListener("click", () => {
+//   axios
+//     .request(options)
+//     .then(function (response) {
+//       console.log(typeof response.data);
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//     });
+// });
+
+// Initialize new SpeechSynthesisUtterance object
+let speech = new SpeechSynthesisUtterance();
+
+// Set Speech Language
+speech.lang = "en";
+
+let voices = []; // global array of available voices
+
+window.speechSynthesis.onvoiceschanged = () => {
+  // Get List of Voices
+  voices = window.speechSynthesis.getVoices();
+  console.log(voices)
+
+  // Initially set the First Voice in the Array.
+  speech.voice = voices[0];
+
+  // Set the Voice Select List. (Set the Index as the value, which we'll use later when the user updates the Voice using the Select Menu.)
+  let voiceSelect = document.querySelector("#voices");
+  voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
 
+document.querySelector("#rate").addEventListener("input", () => {
+  // Get rate Value from the input
+  const rate = document.querySelector("#rate").value;
 
+  // Set rate property of the SpeechSynthesisUtterance instance
+  speech.rate = rate;
 
-btn.addEventListener("click", () => {
-    fetch(
-      "https://voicerss-text-to-speech.p.rapidapi.com/?key=7d510295baf84983bc8d1e26773f7b22",
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => console.error(err));
-  })
+  // Update the rate label
+  document.querySelector("#rate-label").innerHTML = rate;
+});
+
+document.querySelector("#volume").addEventListener("input", () => {
+  // Get volume Value from the input
+  const volume = document.querySelector("#volume").value;
+
+  // Set volume property of the SpeechSynthesisUtterance instance
+  speech.volume = volume;
+
+  // Update the volume label
+  document.querySelector("#volume-label").innerHTML = volume;
+});
+
+document.querySelector("#pitch").addEventListener("input", () => {
+  // Get pitch Value from the input
+  const pitch = document.querySelector("#pitch").value;
+
+  // Set pitch property of the SpeechSynthesisUtterance instance
+  speech.pitch = pitch;
+
+  // Update the pitch label
+  document.querySelector("#pitch-label").innerHTML = pitch;
+});
+
+document.querySelector("#voices").addEventListener("change", () => {
+  // On Voice change, use the value of the select menu (which is the index of the voice in the global voice array)
+  speech.voice = voices[document.querySelector("#voices").value];
+});
+
+document.querySelector("#start").addEventListener("click", () => {
+  // Set the text property with the value of the textarea
+  speech.text = document.querySelector("textarea").value;
+
+  // Start Speaking
+  window.speechSynthesis.speak(speech);
+});
+
+document.querySelector("#pause").addEventListener("click", () => {
+  // Pause the speechSynthesis instance
+  window.speechSynthesis.pause();
+});
+
+document.querySelector("#resume").addEventListener("click", () => {
+  // Resume the paused speechSynthesis instance
+  window.speechSynthesis.resume();
+});
+
+document.querySelector("#cancel").addEventListener("click", () => {
+  // Cancel the speechSynthesis instance
+  window.speechSynthesis.cancel();
+});
